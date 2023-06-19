88a44863bfe2901dc4cfbfc9713c0a4eae6caf15
malok <malok@LAPTOP-1BLM5OS8.mshome.net>
2023-06-19 12:00:35 +0530

    Extent Report

--- src/test/java/day21/Demo1.java
+++ src/test/java/day21/Demo1.java
@@ -11,21 +11,33 @@ import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class Demo1 {
 
-	@Parameters({"gridURL"})
+	@Parameters({"gridURL","appURL"})
 	@Test
-	public void testA(String gridURL) throws Exception
+	public void testA(String gridURL,String appURL) throws Exception
 	{
-		Reporter.log("testA of Demo1 of day 21",true);
+		ExtentReports report=new ExtentReports();	
+		ExtentSparkReporter spark = new ExtentSparkReporter("report/Spark.html");
+		report.attachReporter(spark);
+		ExtentTest extentTest = report.createTest("testA");
+		extentTest.info("testA of Demo1 of day 21");
 		WebDriver driver=new RemoteWebDriver(new URL(gridURL),new ChromeOptions());
-		driver.get("https://demo.actitime.com/login.do");
-		System.out.println(driver.getTitle());
+		driver.get(appURL);
+
+		extentTest.info(driver.getTitle());
+		
+		extentTest.info("Enter username, pw & click login");
 		driver.findElement(By.id("username")).sendKeys("admin");
 		driver.findElement(By.name("pwd")).sendKeys("manager");
 		driver.findElement(By.id("loginButton")).click();
 		Thread.sleep(7000);
-		System.out.println(driver.getTitle());
+		extentTest.info(driver.getTitle());
 		driver.quit();
+		report.flush();
 	}
 }
